0f257993501eaabfe880ae772ff9d10a9f037a63
TheScarastic <warabhishek@gmail.com>
2020-01-25 13:19:35 +0000

    fajita: fod: bump hal to version 1.1
    * Needs sepolicy commit in sdm845-common
    
    Change-Id: Iab7909b797eb1c049defc403c71bbd4cfecfe15d

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -33,6 +33,9 @@
 #define OP_DISPLAY_NOTIFY_PRESS 9
 #define OP_DISPLAY_SET_DIM 10
 
+#define HBM_OFF_DELAY 50
+#define HBM_ON_DELAY 250
+
 // This is not a typo by me. It's by OnePlus.
 #define HBM_ENABLE_PATH "/sys/class/drm/card0-DSI-1/op_friginer_print_hbm"
 #define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/dim_alpha"
@@ -42,7 +45,7 @@ namespace lineage {
 namespace biometrics {
 namespace fingerprint {
 namespace inscreen {
-namespace V1_0 {
+namespace V1_1 {
 namespace implementation {
 
 /*
@@ -64,6 +67,7 @@ static T get(const std::string& path, const T& def) {
 }
 
 FingerprintInscreen::FingerprintInscreen() {
+    int i;
     this->mFodCircleVisible = false;
     this->mVendorFpService = IVendorFingerprintExtensions::getService();
     this->mVendorDisplayService = IOneplusDisplay::getService();
@@ -82,19 +86,38 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
     return Void();
 }
 
+Return<int32_t> FingerprintInscreen::getHbmOffDelay() {
+    return HBM_OFF_DELAY;
+}
+
+Return<int32_t> FingerprintInscreen::getHbmOnDelay() {
+    return HBM_ON_DELAY;
+}
+
+Return<bool> FingerprintInscreen::supportsAlwaysOnHBM() {
+    return true;
+}
+
+Return<void> FingerprintInscreen::switchHbm(bool enabled) {
+    if (enabled) {
+        this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
+        this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+        set(HBM_ENABLE_PATH, 1);
+    } else {
+        this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
+        this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+        set(HBM_ENABLE_PATH, 0);
+    }
+    return Void();
+}
+
 Return<void> FingerprintInscreen::onPress() {
-    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
-    set(HBM_ENABLE_PATH, 1);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
 
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
-    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
-    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
-    set(HBM_ENABLE_PATH, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
@@ -187,7 +210,7 @@ Return<int32_t> FingerprintInscreen::getSize() {
 }
 
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_1
 }  // namespace inscreen
 }  // namespace fingerprint
 }  // namespace biometrics
