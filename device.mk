7d2d269eb49dbf649b090b0721c52357fc40fa93
Dan Cartier <nospamdan1@gmail.com>
2020-03-22 10:23:28 -0400

    fajita: Introduce FP custom icons and animations

--- device.mk
+++ device.mk
@@ -43,5 +43,8 @@ PRODUCT_PACKAGES += \
     vendor.oneplus.fingerprint.extension@1.0 \
     vendor.oneplus.hardware.display@1.0
 
+# FOD animations
+EXTRA_FOD_ANIMATIONS := true
+
 # Inherit from oneplus sdm845-common
 $(call inherit-product, device/oneplus/sdm845-common/common.mk)
